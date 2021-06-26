46709e2528f49e2f589880d4b7c06d11936e24e6
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-06-26 09:10:03 +0530

    Update node.groovy

--- pipeline/node.groovy
+++ pipeline/node.groovy
@@ -1,6 +1,12 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder 
+
 node {
 
 stage ('echoing name'){
   sh """ echo sachin"""
   }
   }
+ return this 
+
