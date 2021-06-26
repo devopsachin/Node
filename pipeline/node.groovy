ff2489b1feae99fd3e1d723f6d0fc034c152959e
sachin_ur <32759077+devopsachin@users.noreply.github.com>
2021-06-26 09:21:08 +0530

    Update node.groovy

--- pipeline/node.groovy
+++ pipeline/node.groovy
@@ -3,9 +3,15 @@ import groovy.json.JsonOutput
 import groovy.json.JsonBuilder 
 
 node {
+  
+  def rootdir="/home/ubuntu"
 
 stage ('echoing name'){
   sh """ echo sachin"""
+  }
+  stage ('cloning reposatry from github'){ 
+    sh """cd ${rootdir} """
+    sh """ git clone https://github.com/devopsachin/Node.git """
   }
   }
  return this 
